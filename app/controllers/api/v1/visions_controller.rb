bdcc8faa1347019a92539635e17b867495b5d5eb
gracenak <gracenak@gmail.com>
2021-02-19 22:17:01 -0700

    add update route

--- app/controllers/api/v1/visions_controller.rb
+++ app/controllers/api/v1/visions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::VisionsController < ApplicationController
+    before_action :set_vision, only: [:update]
+
     def index
         visions = Vision.all
         render json: VisionSerializer.new(visions)
@@ -11,6 +13,26 @@ class Api::V1::VisionsController < ApplicationController
         else
             render json: {errors: vision.errors.full_messages}, status: :unprocessible_entity
         end
+    end
+
+    def update
+        if @vision.update(vision_params)
+            render json: @vision, status:accepted
+        else
+            render json: {errors: @vision.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        vision = Vision.find(params[:id])
+        vision.delete
+        render json: vision
+    end
+
+    private
+
+    def set_vision
+        @vision = Vision.find_by(params[:id])
     end
 
     def vision_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :visions, only: [:index, :create]
+      resources :visions, only: [:index, :create, :update, :destroy]
       resources :themes, only: [:index]
     end
   end
