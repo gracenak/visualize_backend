efb8c9ce5b17b231be54c6e345234404d944a98e
gracenak <gracenak@gmail.com>
2021-06-20 13:46:48 -0700

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,6 @@ other = Theme.create(name: "Other")
 growth_vision = Vision.create(title: "Growth Mindset", description: "'Great works are performed, not by strength, but by perseverance.' -- Samuel Johnson", image_url: "https://i.pinimg.com/564x/6f/ef/f7/6feff7eefa1becebabd632647f318f5a.jpg", theme_id: growth_mindset.id)
 empowerment_vision = Vision.create(title: "Female Empowerment", description: "'Think like a queen. A queen is not afraid to fail. Failure is another stepping stone to greatness.' -– Oprah Winfrey", image_url: "https://www.worthy.com/blog/wp-content/uploads/2017/11/conferences_for_women_header.jpg", theme_id: empowerment.id)
 self_love_vision = Vision.create(title: "Be Kind To Yourself", description: "'Owning our story and loving ourselves through that process is the bravest thing that we'll ever do.' -– Brene Brown", image_url: "https://cdn.shopify.com/s/files/1/0070/8362/2518/articles/Self-love-hug_300x300.jpg?v=1548994010", theme_id: self_love.id)
-dream_home_vision = Vision.create(title: "Bougie", description: "'“Life is not about finding yourself. Life is about creating yourself.” —Lolly Daskal", image_url: "https://cdn.wallpaper.com/main/styles/responsive_770w_scale/s3/unknown-2_0.jpg", theme_id: home.id)
+dream_home_vision = Vision.create(title: "Bougie", description: "'Life is not about finding yourself. Life is about creating yourself.' —Lolly Daskal", image_url: "https://cdn.wallpaper.com/main/styles/responsive_770w_scale/s3/unknown-2_0.jpg", theme_id: home.id)
 
 
